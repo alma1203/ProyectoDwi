bbe8dde14cc9d2c5fca6be0d593b60e382ab27ac
Ailynn Montenegro <U22219194@utp.edu.pe>
2025-06-20 19:28:02 -0500

    contactoooo

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
+  styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  enviarConsulta(event: Event): void {
+    event.preventDefault();
+    alert('¡Consulta enviada con éxito!');
+    (event.target as HTMLFormElement).reset();
+  }
 
 }
